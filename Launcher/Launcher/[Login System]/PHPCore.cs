5613266ef1e2421cdfac51ca2cfdbaeb65fd8ba2
Grootloops33 <43176241+Grootloops33@users.noreply.github.com>
2018-12-04 23:26:07 +0100

    Added PHP core
    PHP core stabilises the PHPClient setup to create a single instance of the server's PHP file, it's accessed once, it runs the full script once (excluding functions), then you can use the write function to call functions seperately.

---
+++ Launcher/Launcher/[Login System]/PHPCore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace BPS.Launcher.Networking
+{
+    public class PHPCore
+    {
+        private static HttpWebRequest req;
+        private static Stream reqStream;
+        private static HttpWebResponse response;
+        private static StreamReader reader;
+
+        public static void Initiate(string url)
+        {
+            try
+            {
+                req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "POST";
+                
+                response = (HttpWebResponse)req.GetResponse();
+                reader = new StreamReader(response.GetResponseStream());
+
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Please check your internet, or tell a programmer");
+            }
+        }
+
+        public static void Write(string message)
+        {
+            byte[] postBytes = Encoding.ASCII.GetBytes(message);
+
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.ContentLength = postBytes.Length;
+
+            reqStream = req.GetRequestStream();
+            reqStream.Write(postBytes, 0, postBytes.Length);
+            reqStream.Close();
+
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
