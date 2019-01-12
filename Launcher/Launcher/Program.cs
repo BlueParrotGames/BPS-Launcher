b3967733d54128ff7c94a28769d451064feadaf6
maikel buunen <maikelbuunen2000@outlook.com>
2019-01-12 19:29:46 +0100

    updated a bit

--- Launcher/Launcher/Program.cs
+++ Launcher/Launcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,14 +13,15 @@ namespace Launcher
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            PHPCore.Initiate("http://localhost/Core/NetCore.php");
-            PHPCore.Write("action=Login&username=test1&password=test1");
+            //Initialize phpCore
+            Thread phpCoreThread = new Thread(unused => PHPCore.Initiate("http://blueparrotgames.com/Core/NetCore.php"));
+            phpCoreThread.Start();
+
 
             Application.Run(new Form1());
         }
